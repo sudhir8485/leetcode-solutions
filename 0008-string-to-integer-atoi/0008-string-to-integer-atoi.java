325991d19418ca04fbc4e5ca20a2e9ef3ab2c498
Sudhir Dhawade <163741215+sudhir8485@users.noreply.github.com>
2026-08-05 15:36:25 +0530

    Time: 1 ms (100%), Space: 43.7 MB (76.25%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -4,7 +4,9 @@ class Solution {
         boolean isNegative = false;
 
         s = s.trim();
+
         if(s.length()==0) return 0;
+
         char sign = s.charAt(0);
 
         int i=0;
@@ -15,31 +17,25 @@ class Solution {
         if(sign=='+'){
             i++;
         } 
-        
-        for( ; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch!='0') break;
-        }
 
         long num = 0;
-        String str = "";
-
-
-
+        
         for( ; i<s.length(); i++){
             char ch = s.charAt(i);
 
             if(ch<'0' || ch>'9') break;
 
             num = num*10 + (ch-'0');
-            str += ch;
+
+            if(num>Integer.MAX_VALUE){
+                if(isNegative) return -2147483648;
+                return 2147483647;
+            }
+            
 
         }
 
-        if(str.length()>10 || num>2147483647) {
-            if(isNegative) return -2147483648;
-            return 2147483647;
-        }
+        
 
         if(isNegative) num*=-1;
 
